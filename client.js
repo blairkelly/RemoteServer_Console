7f9b0929fb731c31ae66663a3e3f163c6414e259
Blair Kelly <blairkelly1@gmail.com>
2014-02-25 16:53:18 -0500

    added style to serial params

--- client.js
+++ client.js
@@ -16,7 +16,7 @@ socket_remote.on('serialEvent', function(data) {
 socket_remote.on('serialParams', function(data) {
 	$('.serialParams').html(''); //clear it
 	for(key in data) {
-		var spit = key + ": " + data[key];
+		var spit = "<span class='key'>" + key + "</span>: " + data[key] + "<br/>";
 		$('.serialParams').append(spit);
 	}
 });
